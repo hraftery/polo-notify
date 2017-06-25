e1c5b9f539d3229223c42ed6f1932e4910b3da3a
Heath Raftery <hraftery@gmail.com>
2017-06-25 13:55:45 +1000

    Initial fork commit. Added option for notification via Pushed instead of email. Pushed notification is a summary JSON trade history.

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # polo-notify
-Python3 Script to check Poloniex for trades and send an email via Sendgrid
+Python3 Script to check Poloniex for trades and either send an email via Sendgrid or send a notification via Pushed.
 
-# Download the repo with:
+# Requirements
 
-`git clone https://github.com/mkuendig/polo-notify`
+`python-poloniex` - to install: `pip3 install https://github.com/s4w3d0ff/python-poloniex/archive/v0.4.4.zip`
+`sendgrid` - only for email notification method
 
-# Insert in to the script:
+# Configuration
 
-Poloniex API and Secret  
-Sendgrid API  
-Email address for from and to  
+Change the following values in the configuration section of the script:
+
+1. Poloniex API and Secret  
+1. Set balance reporting to true to get currency balances with the notifications
+1. Update period in seconds
+1. Notification method 'email' or 'pushed'
+1. For email:
+  1. Sendgrid API  
+  1. Email address for from and to  
+1. For Pushed notification:
+  1. Pushed app\_key and app\_secret
 
 # Python
 
--- polo-notify.py
+++ polo-notify.py
@@ -1,61 +1,126 @@
+# Start Configuration
+
+POLONIEX_KEY = 'add_your_poloniex_api_key_here'
+POLONIEX_SECRET = 'add_your_poloniex_api_secret_here'
+
+BALANCE_REPORTING = False
+UPDATE_PERIOD_SECS = 5*60
+
+NOTIFY_METHOD = 'email_or_pushed'
+
+EMAIL_FROM = 'from@email.com'
+EMAIL_TO = 'to@email.com'
+
+PUSHED_APP_KEY = 'add_your_pushed_app_key_here'
+PUSHED_APP_SECRET = 'add_your_pushed_app_secret_here'
+
+# End Configuration
+
 from poloniex import Poloniex, Coach
 import datetime
-import sendgrid
 import os
 import time
 import pprint
-from sendgrid.helpers.mail import *
 import sys
+
+if NOTIFY_METHOD == 'email':
+	import sendgrid
+	from sendgrid.helpers.mail import *
+elif NOTIFY_METHOD == 'pushed':
+	import requests
+else:
+	print("NOTIFY_METHOD must be set to either 'email' or 'pushed'.")
+	sys.exit(1)
+
+
 sys.stdout.flush()
 
 polo = Poloniex()
 myCoach = Coach()
 
-polo.Key = 'add_your_poloniex_api_key_here'
-polo.Secret = 'add_your_poloniex_api_secret_here'
+polo.Key = POLONIEX_KEY
+polo.Secret = POLONIEX_SECRET
 
 polo.public = Poloniex(coach=myCoach)
 polo.private = Poloniex(polo.Key, polo.Secret, coach=myCoach)
 
-var = 1
-while var == 1 :
-
-	balance = polo.private.returnBalances()
-	balanceETH = balance['ETH']
-	balanceBTC = balance['BTC']
-	balanceUSDT = balance['USDT']
-	#balances = polo.private.returnCompleteBalances()
+# Setup done, enter main loop
+while True:
+	if BALANCE_REPORTING:
+		balance = polo.private.returnBalances()
+		balanceETH = balance['ETH']
+		balanceBTC = balance['BTC']
+		balanceUSDT = balance['USDT']
+		#balances = polo.private.returnCompleteBalances()
 
 	current_time = datetime.datetime.now(datetime.timezone.utc)
 	unix_timestamp = current_time.timestamp() # works if Python >= 3.3
-	unix_timestamp_minus_1_hour = unix_timestamp - (60 * 60)
-	tradehistory = polo.private.returnTradeHistory("all", start=unix_timestamp_minus_1_hour)
+	unix_timestamp_minus_1_period = unix_timestamp - (UPDATE_PERIOD_SECS)
+	tradehistory = polo.private.returnTradeHistory("all", start=unix_timestamp_minus_1_period)
 
 	print(datetime.datetime.now())
-	print("I have" , balanceETH ," ETH")
-	print("I have" , balanceBTC ," BTC")
-	print("I have" , balanceUSDT ," USDT")
+	if BALANCE_REPORTING:
+		print("I have" , balanceETH ," ETH")
+		print("I have" , balanceBTC ," BTC")
+		print("I have" , balanceUSDT ," USDT")
 
 	if len(tradehistory) >= 1:
-	
-		print("Trades / Send email")
 		nicetradehistory = pprint.pformat(tradehistory)
 		print (nicetradehistory)
-		sg = sendgrid.SendGridAPIClient(apikey='add_your_sendgrid_api_key_here')
-		from_email = Email("from_email_at_email.com")
-		subject = "Poloniex Status / Order Update"
-		to_email = Email("to_email_at_email.com")
-		content = Content("text/plain", "BTC: " + balanceBTC + "\n\nETH: " + balanceETH + "\n\nUSDT " + balanceUSDT + "\n\n" + nicetradehistory)
-		#content = Content("text/plain", "BTC: ")
-		mail = Mail(from_email, subject, to_email, content)
-		response = sg.client.mail.send.post(request_body=mail.get())
-		print(response.status_code)
-		print(response.body)
-		print(response.headers)
+
+		if NOTIFY_METHOD == 'email':
+			print("Trades / Send email")
+			sg = sendgrid.SendGridAPIClient(apikey='add_your_sendgrid_api_key_here')
+			from_email = Email(EMAIL_FROM)
+			subject = "Poloniex Status / Order Update"
+			to_email = Email(EMAIL_TO)
+			if BALANCE_REPORTING:
+				content = "BTC: " + balanceBTC + "\n\nETH: " + balanceETH + "\n\nUSDT " + balanceUSDT + "\n\n" + nicetradehistory
+			else:
+				content = nicetradehistory
+			email_content = Content("text/plain", content)
+			#content = Content("text/plain", "BTC: ")
+			mail = Mail(from_email, subject, to_email, email_content)
+			response = sg.client.mail.send.post(request_body=mail.get())
+			print(response.status_code)
+			print(response.body)
+			print(response.headers)
+
+		elif NOTIFY_METHOD == 'pushed':
+			print("Trades / Send pushed notification")
+			
+			# Compress history into just the essentials
+			content = ""
+			for market in tradehistory:
+				content += market + ": "
+				trades = dict()
+				for trade in tradehistory[market]:
+					if not trade['type'] in trades:
+						trades[trade['type']] = dict()
+					if not trade['rate'] in trades[trade['type']]:
+						trades[trade['type']][trade['rate']] = 0.0
+					trades[trade['type']][trade['rate']] += float(trade['total'])
+				for type in trades:
+					content += type + "("				 
+					for rate in trades[type]:
+						content += str(trades[type][rate]) + "@" + rate.rstrip("0") + " "
+					content = content.rstrip() # remove trailing space
+					content += ") "
+			content = content.rstrip() # remove tailing space
+
+			payload = {
+				"app_key": PUSHED_APP_KEY,
+				"app_secret": PUSHED_APP_SECRET,
+				"target_type": "app",
+				"content": content
+			}
+			r = requests.post("https://api.pushed.co/1/push", data=payload)
+			print(r.text)
+
 	else:
 		print("No Trades")
 
 	print("sleeping")
-	time.sleep(3600)
+	time.sleep(UPDATE_PERIOD_SECS)
 
 	
